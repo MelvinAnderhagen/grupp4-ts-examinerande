96100b56383f85f5ecd7a39fffceeb215d261c8d
MelvinAnderhagen <anderhagen.melvin@gmail.com>
2026-08-27 10:19:03 +0200

    bokning interface added

--- src/types/bokning.ts
+++ src/types/bokning.ts
@@ -1,1 +1,9 @@
-interface bokning {}
+interface Bokning {
+  id: string;
+  roomId: string;
+  bokningsEmail: string;
+  date: string;
+  startTime: string;
+  endTime: string;
+  status: "confirmed" | "pending" | "cancelled";
+}
