3dd5f9cc7b6d8d532a130c2e3ceb7bac2080c586
Ameer <antheramir@gmail.c>
2026-08-28 18:14:45 +0200

    filtrera bort detalj sida

--- src/pages/RumSida.tsx
+++ src/pages/RumSida.tsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
+
 export function RumSida() {
   return (
     <div>
       <h2>Tillgängliga Grupprum</h2>
       <p>Här visas listan över alla grupprum i biblioteket.</p>
+      <ul>
+        <li>
+          <Link to="/RumDetaljSida">Rum</Link>
+        </li>
+      </ul>
     </div>
   );
 }
